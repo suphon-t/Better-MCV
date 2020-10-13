69844b2e2d8eb3a4813938ea5aa90ef34c6d5ef7
paphonb <paphonb@gmail.com>
2020-10-13 19:15:55 +0700

    Initial commit

---
+++ Better MCV/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Better MCV
+//
+//  Created by Suphon Thanakornpakapong on 13/10/2563 BE.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
---
+++ Better MCVTests/Better_MCVTests.swift
@@ -0,0 +1,33 @@
+//
+//  Better_MCVTests.swift
+//  Better MCVTests
+//
+//  Created by Suphon Thanakornpakapong on 13/10/2563 BE.
+//
+
+import XCTest
+@testable import Better_MCV
+
+class Better_MCVTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
